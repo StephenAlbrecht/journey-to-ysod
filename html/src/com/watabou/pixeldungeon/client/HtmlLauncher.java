e7c4068889ca7cdff87a71435764a99a84aa59eb
Stephen Albrecht <stephen.b.albrecht@gmail.com>
2020-10-22 21:02:40 -0400

    Initial commit

---
+++ html/src/com/watabou/pixeldungeon/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.watabou.pixeldungeon.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.watabou.pixeldungeon.PixelDungeon;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new PixelDungeon();
+        }
+}
